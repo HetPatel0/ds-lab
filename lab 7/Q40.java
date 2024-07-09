3c9d470e3496e7daa763a5e307fa6d9a3297a590
HetPatel0 <142023310+HetPatel0@users.noreply.github.com>
2024-07-09 20:08:06 +0530

    Q 40 complete

--- lab 7/Q40.java
+++ lab 7/Q40.java
@@ -14,19 +14,33 @@ public class Q40 {
         }
     public static void main(String[] args) {
         Stack <Character> s1 = new Stack<>();
-        Stack <Character> s2 = new Stack<>();
+       
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter string");
         String s = sc.nextLine();
         char next = nextChar(s);
         s1.push(next);
-        int counts1 =1;
-        int counts2 = 0;
+        int count_a =0;
+        int count_b = 0;
         while(next != ' '){
-            next = nextChar(s);
-            if (next == s1.peek()){
-                
+      
+            if (next == 'a'){
+                s1.push(next);
+                count_a++;
+                next= nextChar(s);
             }
+            else{
+                count_b++;
+                s1.push(next);
+                next= nextChar(s);
+
+            }
+        }
+        if (count_a == count_b) {
+            System.out.println("Valid");
+        }
+        else{
+            System.out.println("Not valid");
         }
         
     }
